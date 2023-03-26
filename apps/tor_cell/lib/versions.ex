29562f9dc153fa7b362e3c9e6a9cd3610e1c6209
engler <me@emilengler.com>
2023-03-26 18:49:54 +0000

    Implement the TorCell.Versions module.
    FossilOrigin-Name: da3c6164b7b8f6a5ce9a29027352f7ed630779fad62c91e61ae54165f37e7acd

---
+++ apps/tor_cell/lib/versions.ex
@@ -0,0 +1,26 @@
+defmodule TorCell.Versions do
+  defstruct versions: nil
+
+  @doc """
+  Decodes the payload of a VERSIONS TorCell into its internal representation.
+
+  Returns a TorCell.Versions with versions being a list of all versions given
+  in this cell.
+  """
+  def decode(payload) do
+    for <<x::8, y::8 <- payload>> do
+      <<z::16>> = <<x, y>>
+      z
+    end
+  end
+
+  @doc """
+  Encodes a TorCell.Versions into a binary.
+
+  Returns a binary corresponding to the payload of a VERSIONS TorCell.
+  """
+  def encode(cell) do
+    # TODO: Maybe an overflow check here?
+    Enum.join(Enum.map(cell.versions, fn z -> <<z::16>> end))
+  end
+end
