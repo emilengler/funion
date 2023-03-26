1ab20bd104b84b0eb579033315948259900ced34
engler <me@emilengler.com>
2023-03-26 18:59:52 +0000

    Implement a unit test for TorCell.Versions.
    FossilOrigin-Name: 51cf5c96bef579f05164caee39ee77bf6a5349c543ea1e68cc8a29da9fe56daa

---
+++ apps/tor_cell/test/tor_cell_versions_test.exs
@@ -0,0 +1,20 @@
+defmodule TorCellVersionsTest do
+  use ExUnit.Case
+  doctest TorCell.Versions
+
+  test "decodes a TorCell.Versions" do
+    payload = <<1::16, 2::16, 2 ** 16 - 1::16>>
+    cell = TorCell.Versions.decode(payload)
+
+    assert cell.versions == [1, 2, 2 ** 16 - 1]
+  end
+
+  test "encodes a TorCell.Versions" do
+    data =
+      TorCell.Versions.encode(%TorCell.Versions{
+        versions: [1, 2, 2 ** 16 - 1]
+      })
+
+    assert data == <<1::16, 2::16, 2 ** 16 - 1::16>>
+  end
+end
