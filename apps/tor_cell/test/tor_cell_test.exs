2e9b78a6f69f3a94dd9954991b61ffb51987744f
engler <me@emilengler.com>
2023-03-26 18:44:26 +0000

    Implement a unit test for TorCell.
    FossilOrigin-Name: 24fb38c79b70375c959a0c89c3cd92ed51967527c04272ddb82bb841ed4c1a0a

--- apps/tor_cell/test/tor_cell_test.exs
+++ apps/tor_cell/test/tor_cell_test.exs
@@ -1,4 +1,54 @@
 defmodule TorCellTest do
   use ExUnit.Case
   doctest TorCell
+
+  test "encodes a fixed-length cell (4-byte circ_id)" do
+    data =
+      TorCell.encode(
+        %TorCell{
+          circ_id: 2 ** 32 - 1,
+          cmd: :padding,
+          payload: <<0::509*8>>
+        },
+        4
+      )
+
+    assert data == <<2 ** 32 - 1::32>> <> <<0>> <> <<0::509*8>>
+  end
+
+  test "encodes a variable-length cell (2-byte circ_id)" do
+    data =
+      TorCell.encode(
+        %TorCell{
+          circ_id: 1,
+          cmd: :vpadding,
+          payload: <<1, 2, 3>>
+        },
+        2
+      )
+
+    assert data == <<1::16>> <> <<128>> <> <<3::16>> <> <<1, 2, 3>>
+  end
+
+  test "fetches a fixed-length cell (4-byte circ_id)" do
+    data = <<2 ** 32 - 1::32>> <> <<0>> <> <<0::509*8>> <> <<42, 42, 42>>
+    {cell, data} = TorCell.fetch(data, 4)
+
+    assert cell.circ_id == 2 ** 32 - 1
+    assert cell.cmd == :padding
+    assert cell.payload == <<0::509*8>>
+
+    assert data == <<42, 42, 42>>
+  end
+
+  test "fetches a variable-length cell (2-byte circ_id)" do
+    data = <<255::16>> <> <<7>> <> <<300::16>> <> <<42::300*8>> <> <<1, 2, 3>>
+    {cell, data} = TorCell.fetch(data, 2)
+
+    assert cell.circ_id == 255
+    assert cell.cmd == :versions
+    assert cell.payload == <<42::300*8>>
+
+    assert data == <<1, 2, 3>>
+  end
 end
