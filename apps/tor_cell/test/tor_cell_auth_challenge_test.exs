652951ff145b65f60a3bba08aefe5b5787453f4f
engler <me@emilengler.com>
2023-03-26 20:16:39 +0000

    Implement a unit test for TorCell.AuthChallenge.
    FossilOrigin-Name: 703b91d4aa1420a10cd5bab12fe914ef93355be3c3ca3fc10c5bfc7bdb595e6c

---
+++ apps/tor_cell/test/tor_cell_auth_challenge_test.exs
@@ -0,0 +1,29 @@
+defmodule TorCellAuthChallengeTest do
+  use ExUnit.Case
+  doctest TorCell.AuthChallenge
+
+  test "decodes a TorCell.Versions" do
+    payload = <<42::32*8>> <> <<2::16>> <> <<1::16, 3::16>>
+    cell = TorCell.AuthChallenge.decode(payload)
+
+    assert cell.challenge == <<42::32*8>>
+    assert cell.methods == [:rsa_sha256_tlssecret, :ed25519_sha256_rfc5705]
+  end
+
+  test "decodes an empty TorCell.Versions" do
+    payload = <<42::32*8>> <> <<0::16>>
+    cell = TorCell.AuthChallenge.decode(payload)
+
+    assert cell.challenge == <<42::32*8>>
+    assert cell.methods == []
+  end
+
+  test "encodes a TorCell.Versions" do
+    cell = %TorCell.AuthChallenge{
+      challenge: <<42::32*8>>,
+      methods: [:rsa_sha256_tlssecret, :ed25519_sha256_rfc5705]
+    }
+
+    assert TorCell.AuthChallenge.encode(cell) == <<42::32*8>> <> <<2::16>> <> <<1::16, 3::16>>
+  end
+end
