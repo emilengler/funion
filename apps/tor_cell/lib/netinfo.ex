92e91b05ffe14df6362df4fd7bec67b09cc9a10e
engler <me@emilengler.com>
2023-03-29 11:37:44 +0000

    Implement the TorCell.Netinfo module.
    FossilOrigin-Name: 423f3417acdd5fb2505b034fe446e12382ef8276b632b7bbc8aed43d1530cb33

---
+++ apps/tor_cell/lib/netinfo.ex
@@ -0,0 +1,67 @@
+defmodule TorCell.Netinfo do
+  defstruct time: nil,
+            otheraddr: nil,
+            myaddrs: nil
+
+  defp encode_addr(addr) do
+    atype =
+      case length(addr) do
+        4 -> 0x04
+        16 -> 0x06
+      end
+
+    <<atype::8>> <> <<length(addr)::8>> <> Enum.join(Enum.map(addr, fn x -> <<x>> end))
+  end
+
+  defp fetch_addr(payload) do
+    # TODO: Enforce the type
+    <<_::8, payload::binary>> = payload
+    <<alen::8, payload::binary>> = payload
+    <<aval::binary-size(alen), payload::binary>> = payload
+    # TODO: Parse to an ACTUAL IP data type
+    {:binary.bin_to_list(aval), payload}
+  end
+
+  defp fetch_myaddrs(addrs, n, payload) when n > 0 do
+    {addr, payload} = fetch_addr(payload)
+    fetch_myaddrs(addrs ++ [addr], n - 1, payload)
+  end
+
+  defp fetch_myaddrs(addrs, _, payload) do
+    {addrs, payload}
+  end
+
+  defp fetch_myaddrs(payload) do
+    <<n::8, payload::binary>> = payload
+    fetch_myaddrs([], n, payload)
+  end
+
+  @doc """
+  Decodes the payload of a NETINFO TorCell into its internal representation.
+
+  Returns a TorCell.Netinfo with the fields set accordingly.
+  """
+  def decode(payload) do
+    <<time::32, payload::binary>> = payload
+    {otheraddr, payload} = fetch_addr(payload)
+    {myaddrs, _} = fetch_myaddrs(payload)
+
+    %TorCell.Netinfo{
+      time: DateTime.from_unix!(time),
+      otheraddr: otheraddr,
+      myaddrs: myaddrs
+    }
+  end
+
+  @doc """
+  Encodes a TorCell.Netinfo into a binary.
+
+  Returns a binary corresponding to the payload of a NETINFO TorCell.
+  """
+  def encode(cell) do
+    <<DateTime.to_unix(cell.time)::32>> <>
+      encode_addr(cell.otheraddr) <>
+      <<length(cell.myaddrs)>> <>
+      Enum.join(Enum.map(cell.myaddrs, fn x -> encode_addr(x) end))
+  end
+end
