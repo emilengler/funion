2d0447fdd74d2593cdef06c0ae028012cbec93a9
engler <me@emilengler.com>
2023-03-31 16:20:55 +0000

    Implement a unit test for the TorCert.RsaEd25519 module.
    FossilOrigin-Name: d0738e07c73c1f991fa861085b7abb9f7784ecfb28f03fc47a561dd6c202bac1

---
+++ apps/tor_cert/test/tor_cert_rsa_ed25519_test.exs
@@ -0,0 +1,32 @@
+defmodule TorCertRsaEd25519Test do
+  use ExUnit.Case
+  doctest TorCert.RsaEd25519
+
+  test "fetches a TorCert.RsaEd25519" do
+    data =
+      <<0::32*8>> <>
+        <<0::32>> <>
+        <<32::8>> <>
+        <<0::64*8>>
+
+    {cert, data} = TorCert.RsaEd25519.fetch(data)
+
+    assert cert == %TorCert.RsaEd25519{
+             ed25519_key: <<0::32*8>>,
+             expiration_date: DateTime.from_unix!(0),
+             signature: <<0::32*8>>
+           }
+
+    assert data == <<0::32*8>>
+  end
+
+  test "encodes a TorCert.RsaEd25519" do
+    cert = %TorCert.RsaEd25519{
+      ed25519_key: <<0::32*8>>,
+      expiration_date: DateTime.from_unix!(0),
+      signature: <<0::32*8>>
+    }
+
+    assert TorCert.RsaEd25519.encode(cert) == <<0::32*8>> <> <<0::32>> <> <<32::8>> <> <<0::32*8>>
+  end
+end
