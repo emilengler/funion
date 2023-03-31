ec36477ec5034e1375cac0f31cb72c6bd9cc3401
engler <me@emilengler.com>
2023-03-31 14:21:44 +0000

    Implement the TorCert.RsaEd25519 module.
    FossilOrigin-Name: 719914a6148e08c4583e5a858b089d20a9b9bf2b0bc9c9f69dc3deaec41b6426

---
+++ apps/tor_cert/lib/rsa_ed25519/rsa_ed25519.ex
@@ -0,0 +1,43 @@
+defmodule TorCert.RsaEd25519 do
+  @moduledoc """
+  Provides features for handling with the RSA->Ed25519 cross-certificates
+  specified in `cert-spec.txt`.
+  """
+  defstruct ed25519_key: nil,
+            expiration_date: nil,
+            signature: nil
+
+  @doc """
+  Fetches the first RSA->Ed25519 cross-certificate in a binary.
+
+  Returns the internal representation of the found certificate, alongside
+  the remaining data.
+  """
+  def fetch(data) do
+    <<ed25519_key::binary-size(32), data::binary>> = data
+    <<expiration_date::32, data::binary>> = data
+    <<siglen::8, data::binary>> = data
+    <<signature::binary-size(siglen), data::binary>> = data
+
+    {
+      %TorCert.RsaEd25519{
+        ed25519_key: ed25519_key,
+        expiration_date: DateTime.from_unix!(expiration_date),
+        signature: signature
+      },
+      data
+    }
+  end
+
+  @doc """
+  Encodes an RSA->Ed25519 cross-certificate into a binary.
+
+  Returns a binary corresponding to the certificate.
+  """
+  def encode(cert) do
+    <<cert.ed25519_key::binary-size(32)>> <>
+      <<DateTime.to_unix(cert.expiration_date)::32>> <>
+      <<length(cert.signature)::8>> <>
+      cert.signature
+  end
+end
