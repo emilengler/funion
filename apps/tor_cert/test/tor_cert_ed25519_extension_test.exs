311c531433ef314a4b7f2aa600368f8c1b900620
engler <me@emilengler.com>
2023-03-31 14:44:14 +0000

    Implement a unit test for the TorCert.Ed25519.Extension module.
    FossilOrigin-Name: 4914579d62612b173a2e8ea628868b66baca3a5208036d881d750b158a8f4c9f

---
+++ apps/tor_cert/test/tor_cert_ed25519_extension_test.exs
@@ -0,0 +1,48 @@
+defmodule TorCertEd25519ExtensionTest do
+  use ExUnit.Case
+  doctest TorCert.Ed25519.Extension
+
+  test "fetches a TorCert.Ed25519.Extension" do
+    data = <<32::16>> <> <<0x04::8>> <> <<0::8>> <> <<0::32*8>>
+    {ext, data} = TorCert.Ed25519.Extension.fetch(data)
+
+    assert ext.type == :signed_with_ed25519_key
+    assert ext.flags == nil
+    assert ext.data == <<0::32*8>>
+
+    assert data == <<>>
+  end
+
+  test "fetches a TorCert.Ed25519.Extension with remaining data" do
+    data = <<32::16>> <> <<0x04::8>> <> <<1::8>> <> <<0::64*8>>
+    {ext, data} = TorCert.Ed25519.Extension.fetch(data)
+
+    assert ext.type == :signed_with_ed25519_key
+    assert ext.flags == :affects_validation
+    assert ext.data == <<0::32*8>>
+
+    assert data == <<0::32*8>>
+  end
+
+  test "encodes a TorCert.Ed25519.Extension (flags: nil)" do
+    ext = %TorCert.Ed25519.Extension{
+      type: :signed_with_ed25519_key,
+      flags: nil,
+      data: <<0::32*8>>
+    }
+
+    assert TorCert.Ed25519.Extension.encode(ext) ==
+             <<32::16>> <> <<0x04::8>> <> <<0::8>> <> <<0::32*8>>
+  end
+
+  test "encodes a TorCert.Ed25519.Extension (flags: :affects_validation)" do
+    ext = %TorCert.Ed25519.Extension{
+      type: :signed_with_ed25519_key,
+      flags: :affects_validation,
+      data: <<0::32*8>>
+    }
+
+    assert TorCert.Ed25519.Extension.encode(ext) ==
+             <<32::16>> <> <<0x04::8>> <> <<1::8>> <> <<0::32*8>>
+  end
+end
