3a8aac491898738a16d930f4a9cf64c54a13ff6b
engler <me@emilengler.com>
2023-04-04 14:18:55 +0000

    Implement TorProto.Channel.read_cell().
    FossilOrigin-Name: 33a538b46c9ee6f39fa0a035a9ce88cc924dfc8f37ea2215823a6ed1a7992bba

--- apps/tor_proto/lib/channel/channel.ex
+++ apps/tor_proto/lib/channel/channel.ex
@@ -1,7 +1,18 @@
-defstruct TorProto.Channel do
+defmodule TorProto.Channel do
   @moduledoc """
   Implements the Channel (connection) of the Tor protocol.
   """
   defstruct socket: nil,
             version: nil
+
+  defp read_cell(socket, remaining \\ <<>>) do
+    {:ok, data} = :ssl.recv(socket, 0)
+    data = remaining <> data
+
+    try do
+      TorCell.fetch(data)
+    rescue
+      MatchError -> read_cell(socket, remaining <> data)
+    end
+  end
 end
