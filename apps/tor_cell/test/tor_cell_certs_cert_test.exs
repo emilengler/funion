114bc6c67c19a9f02a0e5c3f6079207f6c106bf1
engler <me@emilengler.com>
2023-04-06 19:24:37 +0000

    Remove the TorCell.Certs and TorCell.Certs.Cert unit tests for now.
    FossilOrigin-Name: ed81815a1e8fb289694659a4615265c9f4248db72bfda608b5f9e73fe85726c8

--- apps/tor_cell/test/tor_cell_certs_cert_test.exs
+++ apps/tor_cell/test/tor_cell_certs_cert_test.exs
@@ -1,33 +1,4 @@
 defmodule TorCellCertsCertTest do
   use ExUnit.Case
   doctest TorCell.Certs.Cert
-
-  test "fetches a cert" do
-    payload = <<4>> <> <<0, 2>> <> <<42, 69>>
-    {cert, payload} = TorCell.Certs.Cert.fetch(payload)
-
-    assert cert == %TorCell.Certs.Cert{
-             type: :ed25519_id_signing,
-             cert: <<42, 69>>
-           }
-
-    assert payload == <<>>
-  end
-
-  test "fetches a cert with remaining data" do
-    payload = <<4>> <> <<0, 1>> <> <<42, 69>>
-    {cert, payload} = TorCell.Certs.Cert.fetch(payload)
-
-    assert cert == %TorCell.Certs.Cert{
-             type: :ed25519_id_signing,
-             cert: <<42>>
-           }
-
-    assert payload == <<69>>
-  end
-
-  test "encodes a cert" do
-    cert = %TorCell.Certs.Cert{type: :ed25519_id_signing, cert: <<1, 2, 3>>}
-    assert TorCell.Certs.Cert.encode(cert) == <<4, 0, 3, 1, 2, 3>>
-  end
 end
--- apps/tor_cell/test/tor_cell_certs_test.exs
+++ apps/tor_cell/test/tor_cell_certs_test.exs
@@ -1,26 +1,4 @@
 defmodule TorCellCertsTest do
   use ExUnit.Case
   doctest TorCell.Certs
-
-  test "decodes a CERTS TorCell" do
-    payload = <<2>> <> <<1, 0, 1, 42>> <> <<2, 0, 2, 42, 69>>
-
-    assert TorCell.Certs.decode(payload) == %TorCell.Certs{
-             certs: [
-               %TorCell.Certs.Cert{type: :rsa_link, cert: <<42>>},
-               %TorCell.Certs.Cert{type: :rsa_id, cert: <<42, 69>>}
-             ]
-           }
-  end
-
-  test "encodes a CERTS TorCell" do
-    cell = %TorCell.Certs{
-      certs: [
-        %TorCell.Certs.Cert{type: :rsa_link, cert: <<42>>},
-        %TorCell.Certs.Cert{type: :rsa_id, cert: <<42, 69>>}
-      ]
-    }
-
-    assert TorCell.Certs.encode(cell) == <<2>> <> <<1, 0, 1, 42>> <> <<2, 0, 2, 42, 69>>
-  end
 end
