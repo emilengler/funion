b16079a9de1ef3cbc079b06999d9a62f2878151f
engler <me@emilengler.com>
2023-04-08 14:27:18 +0000

    Implement TorCell.Padding.
    FossilOrigin-Name: 775ed2fd1debb72b05b458f14aa307324e232799e29d4bc5d56f69815370c9d8

---
+++ apps/tor_cell/lib/padding.ex
@@ -0,0 +1,26 @@
+defmodule TorCell.Padding do
+  defstruct padding: nil
+
+  @doc """
+  Decodes the payload of a PADDING TorCell into its internal representation.
+
+  Returns a TorCell.Padding with padding set to the padding bytes.
+  """
+  def decode(payload) do
+    # Just to be 100% sure ;-)
+    <<padding::binary-size(509)>> = payload
+
+    %TorCell.Padding{
+      padding: padding
+    }
+  end
+
+  @doc """
+  Encodes a TorCell.Padding into a binary.
+
+  Returns a binary corresponding to the payload of a PADDING TorCell.
+  """
+  def encode(cell) do
+    <<cell.padding::binary-size(509)>>
+  end
+end
