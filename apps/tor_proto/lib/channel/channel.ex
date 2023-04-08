fba3b7465a6676dd89b1ab93049377a919c91041
engler <me@emilengler.com>
2023-04-08 10:46:51 +0000

    Begin serious work regarding the channel handshake.
    FossilOrigin-Name: 55cc1e4df8d1a63c2231acf3c5ebf7fbb5a78acd8b3f63298858065433af1914

--- apps/tor_proto/lib/channel/channel.ex
+++ apps/tor_proto/lib/channel/channel.ex
@@ -5,14 +5,40 @@ defmodule TorProto.Channel do
   defstruct socket: nil,
             version: nil
 
-  defp read_cell(socket, remaining \\ <<>>) do
-    {:ok, data} = :ssl.recv(socket, 0)
-    data = remaining <> data
+  defp gen_versions_cell() do
+    TorCell.encode(
+      %TorCell{
+        circ_id: 0,
+        cmd: :versions,
+        payload: TorCell.Versions.encode(%TorCell.Versions{versions: [3, 4]})
+      },
+      2
+    )
+  end
 
+  defp recv_cell(socket, circ_id_len, remaining) do
     try do
-      TorCell.fetch(data)
+      TorCell.fetch(remaining, circ_id_len)
     rescue
-      MatchError -> read_cell(socket, remaining <> data)
+      MatchError ->
+        {:ok, new} = :ssl.recv(socket, 0, 5000)
+        recv_cell(socket, circ_id_len, remaining <> :binary.list_to_bin(new))
     end
+  end
+
+  @doc """
+  Initiates a new channel on a connected TLS socket.
+
+  Returns the internal represenation of a Channel.
+  """
+  def initiate(socket) do
+    :ok = :ssl.send(socket, gen_versions_cell())
+
+    {versions_cell, remaining} = recv_cell(socket, 2, <<>>)
+    {certs_cell, remaining} = recv_cell(socket, 4, remaining)
+    {auth_challenge_cell, remaining} = recv_cell(socket, 4, remaining)
+    {netinfo_cell, remaining} = recv_cell(socket, 4, remaining)
+
+    nil
   end
 end
