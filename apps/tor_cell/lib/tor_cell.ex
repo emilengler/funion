041131b73a076a8297a516275e8ecd752540c528
engler <me@emilengler.com>
2023-04-08 16:20:17 +0000

    Refactor TorCell, remove unsupported cmds, and decode the payload.
    FossilOrigin-Name: 5811666e911d9f970e284ad0bd88fc9fc078b8f6e104d19b0daf331802a89c79

--- apps/tor_cell/lib/tor_cell.ex
+++ apps/tor_cell/lib/tor_cell.ex
@@ -9,104 +9,112 @@ defmodule TorCell do
   defp decode_cmd(cmd) do
     case cmd do
       0 -> :padding
-      1 -> :create
-      2 -> :created
-      3 -> :relay
-      4 -> :destroy
-      5 -> :create_fast
-      6 -> :created_fast
       7 -> :versions
       8 -> :netinfo
-      9 -> :relay_early
-      10 -> :create2
-      11 -> :created2
-      12 -> :padding_negotiate
       128 -> :vpadding
       129 -> :certs
       130 -> :auth_challenge
       131 -> :authenticate
-      132 -> :authorize
     end
   end
 
-  defp encode_circ_id(circ_id, circ_id_len) do
-    # Convert from bytes to bits
+  defp encode_circ_id_len(circ_id, circ_id_len) do
+    # Convet circ_id_len from bytes to bits
     circ_id_len = circ_id_len * 8
 
-    # TODO: Catch overflow
     <<circ_id::integer-size(circ_id_len)>>
   end
 
   defp encode_cmd(cmd) do
-    <<case cmd do
-        :padding -> 0
-        :create -> 1
-        :created -> 2
-        :relay -> 3
-        :destroy -> 4
-        :create_fast -> 5
-        :created_fast -> 6
-        :versions -> 7
-        :netinfo -> 8
-        :relay_early -> 9
-        :create2 -> 10
-        :created2 -> 11
-        :padding_negotiate -> 12
-        :vpadding -> 128
-        :certs -> 129
-        :auth_challenge -> 130
-        :authenticate -> 131
-        :authorize -> 132
-      end>>
+    case cmd do
+      :padding -> <<0>>
+      :versions -> <<7>>
+      :netinfo -> <<8>>
+      :vpadding -> <<128>>
+      :certs -> <<129>>
+      :auth_challenge -> <<130>>
+      :authenticate -> <<131>>
+    end
   end
 
-  defp encode_payload(payload, vlen) do
-    if vlen do
-      # TODO: Check overflow
+  defp encode_payload(payload, cmd) do
+    payload =
+      case cmd do
+        :padding -> TorCell.Padding.encode(payload)
+        :versions -> TorCell.Versions.encode(payload)
+        :netinfo -> TorCell.Netinfo.encode(payload)
+        :vpadding -> TorCell.Vpadding.encode(payload)
+        :certs -> TorCell.Certs.encode(payload)
+        :auth_challenge -> TorCell.AuthChallenge.encode(payload)
+        :authenticate -> TorCell.Authenticate.encode(payload)
+      end
+
+    # Fill the payload with padding bytes
+    # TODO: Consider doing this in the TorCell.XXX.encode() functions
+    payload =
+      if !is_vlen?(cmd) do
+        padding = 509 - byte_size(payload)
+        payload <> <<0::padding*8>>
+      else
+        payload
+      end
+
+    if is_vlen?(cmd) do
       <<byte_size(payload)::16>> <> payload
     else
-      <<payload::binary-size(509)>>
+      payload
     end
   end
 
   defp fetch_circ_id(data, circ_id_len) do
-    # Convert from bytes to bits
+    # Convert circ_id_len from bytes to bits
     circ_id_len = circ_id_len * 8
 
     <<circ_id::integer-size(circ_id_len), data::binary>> = data
     {circ_id, data}
   end
 
-  defp fetch_cmd(data) do
-    <<cmd, data::binary>> = data
-    {decode_cmd(cmd), data}
-  end
+  defp fetch_payload(data, cmd) do
+    # TODO: Consider splitting this into two functions
+    {payload, data} =
+      if is_vlen?(cmd) do
+        <<length::16, data::binary>> = data
+        <<payload::binary-size(length), data::binary>> = data
+        {payload, data}
+      else
+        <<payload::binary-size(509), data::binary>> = data
+        {payload, data}
+      end
 
-  defp fetch_payload(data, vlen) do
-    if vlen do
-      <<length::16, data::binary>> = data
-      <<payload::binary-size(length), data::binary>> = data
-      {payload, data}
-    else
-      <<payload::binary-size(509), data::binary>> = data
-      {payload, data}
-    end
+    payload =
+      case cmd do
+        :padding -> TorCell.Padding.decode(payload)
+        :versions -> TorCell.Versions.decode(payload)
+        :netinfo -> TorCell.Netinfo.decode(payload)
+        :vpadding -> TorCell.Vpadding.decode(payload)
+        :certs -> TorCell.Certs.decode(payload)
+        :auth_challenge -> TorCell.AuthChallenge.decode(payload)
+        :authenticate -> TorCell.Authenticate.decode(payload)
+      end
+
+    {payload, data}
   end
 
   defp is_vlen?(cmd) do
-    cmd in [:versions, :vpadding, :certs, :auth_challenge, :authenticate, :authorize]
+    cmd in [:versions, :vpadding, :certs, :auth_challenge, :authenticate]
   end
 
   @doc """
-  Fetches the first Cell in a binary.
+  Fetches the first TorCell in a binary.
 
-  Returns the internal representation of the found Cell, alongside the
-  remaining data.
+  Returns the internal representation of the found TorCell, alongside
+  the remaining data.
   """
   def fetch(data, circ_id_len \\ 4) do
     {circ_id, data} = fetch_circ_id(data, circ_id_len)
-    {cmd, data} = fetch_cmd(data)
-    {payload, data} = fetch_payload(data, is_vlen?(cmd))
+    <<cmd, data::binary>> = data
+    cmd = decode_cmd(cmd)
+    {payload, data} = fetch_payload(data, cmd)
 
     {
       %TorCell{
@@ -120,10 +128,12 @@ defmodule TorCell do
 
   @doc """
   Encodes the TorCell into a binary.
+
+  Returns a binary corresponding to the TorCell.
   """
   def encode(cell, circ_id_len \\ 4) do
-    encode_circ_id(cell.circ_id, circ_id_len) <>
+    encode_circ_id_len(cell.circ_id, circ_id_len) <>
       encode_cmd(cell.cmd) <>
-      encode_payload(cell.payload, is_vlen?(cell.cmd))
+      encode_payload(cell.payload, cell.cmd)
   end
 end
--- apps/tor_cell/test/tor_cell_test.exs
+++ apps/tor_cell/test/tor_cell_test.exs
@@ -8,7 +8,7 @@ defmodule TorCellTest do
         %TorCell{
           circ_id: 2 ** 32 - 1,
           cmd: :padding,
-          payload: <<0::509*8>>
+          payload: %TorCell.Padding{padding: <<0::509*8>>}
         },
         4
       )
@@ -22,7 +22,7 @@ defmodule TorCellTest do
         %TorCell{
           circ_id: 1,
           cmd: :vpadding,
-          payload: <<1, 2, 3>>
+          payload: %TorCell.Vpadding{padding: <<1, 2, 3>>}
         },
         2
       )
@@ -37,20 +37,22 @@ defmodule TorCellTest do
     assert cell == %TorCell{
              circ_id: 2 ** 32 - 1,
              cmd: :padding,
-             payload: <<0::509*8>>
+             payload: %TorCell.Padding{padding: <<0::509*8>>}
            }
 
     assert data == <<42, 42, 42>>
   end
 
   test "fetches a variable-length cell (2-byte circ_id)" do
-    data = <<255::16>> <> <<7>> <> <<300::16>> <> <<42::300*8>> <> <<1, 2, 3>>
+    data =
+      <<255::16>> <> <<7>> <> <<10::16>> <> <<1::16, 2::16, 3::16, 4::16, 5::16>> <> <<1, 2, 3>>
+
     {cell, data} = TorCell.fetch(data, 2)
 
     assert cell == %TorCell{
              circ_id: 255,
              cmd: :versions,
-             payload: <<42::300*8>>
+             payload: %TorCell.Versions{versions: [1, 2, 3, 4, 5]}
            }
 
     assert data == <<1, 2, 3>>
