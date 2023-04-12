cba1e8a5290a277e4e3c69b5348c79a3ffd7df45
engler <me@emilengler.com>
2023-04-12 18:02:52 +0000

    Implement a unit test for the TorCell.Create2 module.
    FossilOrigin-Name: 7368c671cacf2e4a784b9200cecc8974f39e33924b5aaa9c243dff45fde1ed1b

---
+++ apps/tor_cell/test/tor_cell_create2_test.exs
@@ -0,0 +1,18 @@
+defmodule TorCellCreate2Test do
+  use ExUnit.Case
+  doctest TorCell.Create2
+
+  test "decodes a CREATE2 TorCell" do
+    assert TorCell.Create2.decode(<<0, 2, 0, 2, 42, 69>>) == %TorCell.Create2{
+             type: :ntor,
+             data: <<42, 69>>
+           }
+  end
+
+  test "encodes a CREATE2 TorCell" do
+    assert TorCell.Create2.encode(%TorCell.Create2{
+             type: :ntor,
+             data: <<42, 69>>
+           }) == <<0, 2, 0, 2, 42, 69>>
+  end
+end
