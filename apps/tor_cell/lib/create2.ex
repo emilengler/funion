df71deca6569926be740c073f266350dc1df3966
engler <me@emilengler.com>
2023-04-12 16:59:24 +0000

    Implement the TorCell.Create2 module.
    FossilOrigin-Name: 598075f4673a8c5fab583e6a36160b30fbec9b3a8fc9c71bde6f307204e3731e

---
+++ apps/tor_cell/lib/create2.ex
@@ -0,0 +1,31 @@
+defmodule TorCell.Create2 do
+  defstruct type: nil,
+            data: nil
+
+  @doc """
+  Decodes the payload of a CREATE2 TorCell into its internal representation.
+
+  Returns a TorCell.Create2 with type and data set accordingly.
+  """
+  def decode(payload) do
+    <<0x02::16, payload::binary>> = payload
+    <<len::16, payload::binary>> = payload
+    <<data::binary-size(len), _>> = payload
+
+    %TorCell.Create2{
+      type: :ntor,
+      data: data
+    }
+  end
+
+  @doc """
+  Encodes a TorCell.Create2 into a binary.
+
+  Returns a binary corresponding to the payload of a CREATE2 TorCell.
+  """
+  def encode(cell) do
+    :ntor = cell.type
+
+    <<0x02::16>> <> <<byte_size(cell.data)::16>> <> <<cell.data>>
+  end
+end
