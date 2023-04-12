ecd08773f3770738f86cd763e4283bff0da89fe0
engler <me@emilengler.com>
2023-04-12 17:30:38 +0000

    Do better comparisons in the TorCell.Authenticate unit test.
    FossilOrigin-Name: 3629d2dc417adbbf9ea0ab627b781018600b402d38e88aa68bd01dd9dde8f9e7

--- apps/tor_cell/test/tor_cell_authenticate_test.exs
+++ apps/tor_cell/test/tor_cell_authenticate_test.exs
@@ -12,17 +12,18 @@ defmodule TorCellAuthenticate do
     payload = <<1::16>> <> <<byte_size(auth)::16>> <> auth
     cell = TorCell.Authenticate.decode(payload)
 
-    assert cell.type == :rsa_sha256_tlssecret
-
-    assert cell.auth == %TorCell.Authenticate.RsaSha256Tlssecret{
-             cid: <<0::8*32>>,
-             sid: <<1::8*32>>,
-             slog: <<2::8*32>>,
-             clog: <<3::8*32>>,
-             scert: <<4::8*32>>,
-             tlssecrets: <<5::8*32>>,
-             rand: <<6::8*24>>,
-             sig: <<42, 69>>
+    assert cell == %TorCell.Authenticate{
+             type: :rsa_sha256_tlssecret,
+             auth: %TorCell.Authenticate.RsaSha256Tlssecret{
+               cid: <<0::8*32>>,
+               sid: <<1::8*32>>,
+               slog: <<2::8*32>>,
+               clog: <<3::8*32>>,
+               scert: <<4::8*32>>,
+               tlssecrets: <<5::8*32>>,
+               rand: <<6::8*24>>,
+               sig: <<42, 69>>
+             }
            }
   end
 
@@ -45,17 +46,20 @@ defmodule TorCellAuthenticate do
 
     assert cell.type == :ed25519_sha256_rfc5705
 
-    assert cell.auth == %TorCell.Authenticate.Ed25519Sha256Rfc5705{
-             cid: <<0::8*32>>,
-             sid: <<1::8*32>>,
-             cid_ed: <<2::8*32>>,
-             sid_ed: <<3::8*32>>,
-             slog: <<4::8*32>>,
-             clog: <<5::8*32>>,
-             scert: <<6::8*32>>,
-             tlssecrets: <<7::8*32>>,
-             rand: <<8::8*24>>,
-             sig: <<42, 69>>
+    assert cell == %TorCell.Authenticate{
+             type: :ed25519_sha256_rfc5705,
+             auth: %TorCell.Authenticate.Ed25519Sha256Rfc5705{
+               cid: <<0::8*32>>,
+               sid: <<1::8*32>>,
+               cid_ed: <<2::8*32>>,
+               sid_ed: <<3::8*32>>,
+               slog: <<4::8*32>>,
+               clog: <<5::8*32>>,
+               scert: <<6::8*32>>,
+               tlssecrets: <<7::8*32>>,
+               rand: <<8::8*24>>,
+               sig: <<42, 69>>
+             }
            }
   end
 
