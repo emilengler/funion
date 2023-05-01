ce34fe5d9c394b846e1e99649b71911fec7626c6
engler <me@emilengler.com>
2023-05-01 13:44:56 +0000

    Perform the scalar multiplication properly.
    FossilOrigin-Name: a56421b17bd106fc5928bd9e496fd112e8cee29bb408b37c99fa22f1a286efe8

--- apps/tor_crypto/lib/handshake/ntor/client.ex
+++ apps/tor_crypto/lib/handshake/ntor/client.ex
@@ -23,20 +23,12 @@ defmodule TorCrypto.Handshake.Ntor.Client do
   Returns a binary corresponding to the secret input.
   """
   def stage3(b, id, x_pk, x_sk, y) do
-    # TODO: Remove the boilerplate
-    # The cryptographic keys as real integers
-    <<b::integer-size(32)-unit(8)>> = b
-    <<id::integer-size(20)-unit(8)>> = id
-    <<x_pk::integer-size(32)-unit(8)>> = x_pk
-    <<x_sk::integer-size(32)-unit(8)>> = x_sk
-    <<y::integer-size(32)-unit(8)>> = y
-
-    :binary.encode_unsigned(y * x_sk) <>
-      :binary.encode_unsigned(b * x_sk) <>
-      :binary.encode_unsigned(id) <>
-      :binary.encode_unsigned(b) <>
-      :binary.encode_unsigned(x_pk) <>
-      :binary.encode_unsigned(y) <>
+    :crypto.compute_key(:ecdh, y, x_sk, :x25519) <>
+      :crypto.compute_key(:ecdh, b, x_sk, :x25519) <>
+      id <>
+      b <>
+      x_pk <>
+      y <>
       "ntor-curve25519-sha256-1"
   end
 end
