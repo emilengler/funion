78646f07b1602ae964051f90c039ee0561872245
engler <me@emilengler.com>
2023-05-06 15:49:08 +0000

    Begin work on the TorCrypto.Digest module.
    FossilOrigin-Name: 76640d6aff6adf1be6fe1aaeb5fb11b33ad008266e620e2bb48551746cce34f8

---
+++ apps/tor_crypto/lib/digest.ex
@@ -0,0 +1,32 @@
+defmodule TorCrypto.Digest do
+  @moduledoc """
+  Implements the running digest in a stateful fashion.
+  """
+
+  @doc """
+  Initializes the state required for calculating the running digest.
+
+  Returns the internal representation of the state.
+  """
+  def init() do
+    :crypto.hash_init(:sha)
+  end
+
+  @doc """
+  Updates the state with new data.
+
+  Returns a new internal representation of the state, obsoleting the previous.
+  """
+  def update(state, data) do
+    :crypto.hash_update(state, data)
+  end
+
+  @doc """
+  Calculates the actual digest of the state.
+
+  Returns a binary corresponding to the current digest of the state.
+  """
+  def calculate(state) do
+    :crypto.hash_final(state)
+  end
+end
