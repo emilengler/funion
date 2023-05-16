8342748332529f027bab12d07c541615c8a64540
engler <me@emilengler.com>
2023-05-16 18:07:17 +0000

    Refactor the decryption logic of TorCell.Relay.Unencrypted.
    FossilOrigin-Name: ef0ed16a5ac853ddac2ad7d1918b02c2781b2a78e8e2056dda99caf85d5ff02a

--- apps/tor_cell/lib/relay/unencrypted.ex
+++ apps/tor_cell/lib/relay/unencrypted.ex
@@ -1,6 +1,45 @@
 defmodule TorCell.Relay.Unencrypted do
   defstruct cmd: nil,
             stream_id: nil,
-            payload: nil,
-            padding: nil
+            data: nil
+
+  defp decode(payload, context) do
+    <<cmd, padding::binary>> = payload
+    <<recognized::16, padding::binary>> = padding
+    <<stream_id::16, padding::binary>> = padding
+    <<digest::binary-size(4), padding::binary>> = padding
+    <<length::16, padding::binary>> = padding
+    <<data::binary-size(length), padding::binary>> = padding
+    true = byte_size(padding) == 509 - 11 - length
+
+    # The new context with the (unencrypted) payload of this cell
+    new_context = TorCrypto.Digest.update(context, payload)
+
+    if is_decrypted?(recognized, digest, new_context) do
+      {
+        true,
+        %TorCell.Relay.Unencrypted{cmd: cmd, stream_id: stream_id, data: data},
+        new_context
+      }
+    else
+      {false, %TorCell.Relay{onion_skin: payload}, context}
+    end
+  end
+
+  defp is_decrypted?(recognized, digest, context) do
+    recognized == 0 && is_valid_digest?(digest, context)
+  end
+
+  defp is_valid_digest?(digest, context) do
+    <<TorCrypto.Digest.calculate(context)::binary-size(4)>> == digest
+  end
+
+  @doc """
+  Decrypts a TorCell.Relay by removing length(keys) onion skins from it.
+
+  TODO: Document return values
+  """
+  def decrypt(cell, context, keys) do
+    decode(TorCrypto.OnionSkin.decrypt(cell.onion_skin, keys), context)
+  end
 end
