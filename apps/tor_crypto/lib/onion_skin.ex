0e0d0ad5993781047cb0eba7289f0a0010b10232
engler <me@emilengler.com>
2023-05-19 15:00:21 +0000

    Create the onion skins as specified in the Tor specification.
    FossilOrigin-Name: f4be09ba671caa739f2540bab2db39b0ff3c9227c424ce51708d99a2e87565a1

--- apps/tor_crypto/lib/onion_skin.ex
+++ apps/tor_crypto/lib/onion_skin.ex
@@ -1,14 +1,15 @@
 defmodule TorCrypto.OnionSkin do
   @doc """
-  Decrypts an onion skin layer by layer from a list of keys.
-  The onion skin is decrypted by the keys in the list from last to first.
+  Decrypts an onion skin, layer by layer from a list of keys, as
+  specified in 5.5.3 in `tor-spec.txt`.
+  The onion skin is decrypted from the first to the last key.
 
   Returns a binary corresponding to the decrypted onion skin.
   """
   def decrypt(data, keys) when length(keys) > 0 do
     decrypt(
-      :crypto.crypto_one_time_aead(:aes_128_gcm, List.last(keys), <<0>>, data, <<>>, false),
-      Enum.take(keys, length(keys) - 1)
+      :crypto.crypto_one_time_aead(:aes_128_gcm, hd(keys), <<0>>, data, <<>>, false),
+      tl(keys)
     )
   end
 
@@ -17,14 +18,17 @@ defmodule TorCrypto.OnionSkin do
   end
 
   @doc """
-  Encrypts data to an onion skin layer by layer from a list of keys.
-  The onion skin is encrpyted by the keys in the list from first to last.
+  Encrypts data to an onion skin, layer by layer from a list of keys, as
+  specified in 5.5.2.1 in `tor-spec.txt`.
+  The onion skin is created from the last to the first key.
 
-  Returns a binary corresponding to the encrypted onion skins.
+  Returns a binary corresponding to the encrypted onion skin.
   """
   def encrypt(data, keys) when length(keys) > 0 do
-    {encrypted, _} = :crypto.crypto_one_time_aead(:aes_128_gcm, hd(keys), <<0>>, data, <<>>, true)
-    encrypt(encrypted, tl(keys))
+    {encrypted, _} =
+      :crypto.crypto_one_time_aead(:aes_128_gcm, List.last(keys), <<0>>, data, <<>>, true)
+
+    encrypt(encrypted, Enum.take(keys, length(keys - 1)))
   end
 
   def encrypt(data, _) do
