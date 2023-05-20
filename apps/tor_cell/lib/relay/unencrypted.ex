ed6ad5e7265d4440b34947c877b8a412531bdb82
engler <me@emilengler.com>
2023-05-20 12:09:54 +0000

    Properly calculate the digest by taking the current cell into account.
    FossilOrigin-Name: 9adcbeec01a7b41d00504e8a286f104a84c08ed6820c86408799f6a5bf26b0f1

--- apps/tor_cell/lib/relay/unencrypted.ex
+++ apps/tor_cell/lib/relay/unencrypted.ex
@@ -28,14 +28,14 @@ defmodule TorCell.Relay.Unencrypted do
     data = decode_data(cmd, data)
     true = byte_size(padding) == 509 - 11 - length
 
-    # The new context with the (unencrypted) payload of this cell
-    new_context = TorCrypto.Digest.update(context, payload)
+    # The context with this cell's payload but digest set to zero
+    tmp_context = TorCrypto.Digest.update(context, modify_digest(payload, <<0::32>>))
 
-    if is_decrypted?(recognized, digest, new_context) do
+    if is_decrypted?(recognized, digest, tmp_context) do
       {
         true,
         %TorCell.Relay.Unencrypted{cmd: cmd, stream_id: stream_id, data: data},
-        new_context
+        TorCrypto.Digest.update(context, payload)
       }
     else
       {false, %TorCell.Relay{onion_skin: payload}, context}
@@ -60,13 +60,20 @@ defmodule TorCell.Relay.Unencrypted do
     encoded_data = encode_data(cell.cmd, cell.data)
     padding_len = 509 - 11 - byte_size(encoded_data)
 
-    encode_cmd(cell.cmd) <>
-      <<0::16>> <>
-      <<cell.stream_id::16>> <>
-      <<TorCrypto.Digest.calculate(context)::binary-size(4)>> <>
-      <<byte_size(encoded_data)::16>> <>
-      encoded_data <>
-      <<0::integer-size(padding_len)-unit(8)>>
+    encoded =
+      encode_cmd(cell.cmd) <>
+        <<0::16>> <>
+        <<cell.stream_id::16>> <>
+        <<0::32>> <>
+        <<byte_size(encoded_data)::16>> <>
+        encoded_data <>
+        <<0::integer-size(padding_len)-unit(8)>>
+
+    # The context with this cell's payload but digest set to zero
+    tmp_context = TorCrypto.Digest.update(context, encoded)
+    encoded = modify_digest(encoded, <<TorCrypto.Digest.calculate(tmp_context)::binary-size(4)>>)
+
+    {encoded, TorCrypto.Digest.update(context, encoded)}
   end
 
   defp is_decrypted?(recognized, digest, context) do
@@ -75,6 +82,12 @@ defmodule TorCell.Relay.Unencrypted do
 
   defp is_valid_digest?(digest, context) do
     <<TorCrypto.Digest.calculate(context)::binary-size(4)>> == digest
+  end
+
+  defp modify_digest(payload, digest) do
+    <<prefix::binary-size(5), suffix::binary>> = payload
+    <<_::binary-size(4), suffix::binary>> = suffix
+    prefix <> <<digest::binary-size(4)>> <> suffix
   end
 
   @doc """
@@ -92,8 +105,7 @@ defmodule TorCell.Relay.Unencrypted do
   TODO: Document return values
   """
   def encrypt(cell, context, keys) do
-    encoded = encode(cell, context)
-    context = TorCrypto.Digest.update(context, encoded)
+    {encoded, context} = encode(cell, context)
     {%TorCell.Relay{onion_skin: TorCrypto.OnionSkin.encrypt(encoded, keys)}, context}
   end
 end
