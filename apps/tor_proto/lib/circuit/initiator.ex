05fd534b0411a6f9e4f5fd9b7ec8a79eeb1895d3
engler <me@emilengler.com>
2023-05-24 11:37:35 +0000

    Begin work on circuit extension.
    FossilOrigin-Name: 5d44ab70dd7455946602208460ca51a6411e706159e77f7228e0c50da0a193ff

--- apps/tor_proto/lib/circuit/initiator.ex
+++ apps/tor_proto/lib/circuit/initiator.ex
@@ -1,6 +1,8 @@
 defmodule TorProto.Circuit.Initiator do
   @moduledoc """
   Manages an initiator of a circuit on a channel in the Tor protocol.
+
+  TODO: Do the handshakes in some sort of a separate function.
   """
 
   defp recv_cell() do
@@ -18,7 +20,102 @@ defmodule TorProto.Circuit.Initiator do
   end
 
   defp handler(router, circ_id, parent, state) do
-    :ok
+    receive do
+      {:extend, router, pid} ->
+        # Generate the specs field
+        specs = [
+          %TorCell.RelayCell.Extend2.Spec{
+            type: :tls_over_tcp4,
+            spec: %TorCell.RelayCell.Extend2.Spec.TlsOverTcp4{
+              ip: router.ip4,
+              port: router.orport
+            }
+          },
+          %TorCell.RelayCell.Extend2.Spec{
+            type: :legacy_identity,
+            spec: %TorCell.RelayCell.Extend2.Spec.LegacyIdentity{fingerprint: router.identity}
+          },
+          %TorCell.RelayCell.Extend2.Spec{
+            type: :ed25519_identity,
+            spec: %TorCell.RelayCell.Extend2.Spec.Ed25519Identity{
+              fingerprint: router.keys.ed25519_identity
+            }
+          }
+        ]
+
+        # Append a :tls_over_tcp6 spec if this field is set in router
+        specs =
+          if router.ip6 == nil do
+            specs
+          else
+            specs ++
+              [
+                %TorCell.RelayCell.Extend2.Spec{
+                  type: :tls_over_tcp6,
+                  spec: %TorCell.RelayCell.Extend2.Spec.TlsOverTcp6{
+                    ip: router.ip6,
+                    port: router.orport
+                  }
+                }
+              ]
+          end
+
+        # Prepare the ntor handshake
+        {x_pk, x_sk} = TorCrypto.Handshake.Ntor.Client.stage1()
+        b = router.keys.x25519_ntor
+        id = router.identity
+
+        # Generate the cell with the appropriate ntor handshake
+        extend2 = %TorCell.RelayCell{
+          cmd: :extend2,
+          stream_id: 0,
+          data: %TorCell.RelayCell.Extend2{
+            specs: specs,
+            type: :ntor,
+            data: TorCrypto.Handshake.Ntor.Client.stage2(b, id, x_pk)
+          }
+        }
+
+        # Get the onion skin of the extend2 cell
+        cf = state[:context_forward]
+        keys_f = Enum.map(state[:keys], fn x -> x.kf end)
+        {onion_skin, cf} = TorCell.RelayCell.encrypt(extend2, cf, keys_f)
+
+        # Place the extend2 cell into a TorCell
+        extend2 = %TorCell{
+          circ_id: circ_id,
+          cmd: :relay_early,
+          payload: %TorCell.RelayEarly{onion_skin: onion_skin}
+        }
+
+        :ok = send_cell(extend2, parent)
+
+        # Receive an EXTENDED2 TorCell
+        extended2 = recv_cell()
+
+        cb = state[:context_backward]
+        keys_b = Enum.map(state[:keys], fn x -> x.kb end)
+
+        {true, extended2, cb} =
+          TorCell.RelayCell.decrypt(extended2.payload.onion_skin, cb, keys_b)
+
+        %TorCell.RelayCell{cmd: :extended2, data: %TorCell.RelayCell.Extended2{data: data}} =
+          extended2
+
+        # Finalize the handshake
+        <<y::binary-size(32), data::binary>> = data
+        <<auth::binary-size(32), _::binary>> = data
+
+        # TODO: Check if Y is in G^
+
+        secret_input = TorCrypto.Handshake.Ntor.Client.stage3(b, id, x_pk, x_sk, y)
+        true = TorCrypto.Handshake.Ntor.Client.is_valid?(secret_input, auth, b, id, x_pk, y)
+        keys = TorCrypto.Handshake.Ntor.derive_keys(secret_input)
+
+        send(pid, {:extend, :ok})
+
+        # TODO: Update the state
+    end
   end
 
   @doc """
