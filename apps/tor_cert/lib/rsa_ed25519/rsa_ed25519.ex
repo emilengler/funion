76fb1a19e27176afba2c898a3bc43828cb8ee887
engler <me@emilengler.com>
2023-06-25 15:24:11 +0000

    Decode/Encode the expiration date in hours, not in seconds.
    FossilOrigin-Name: 9c1a3190d01167bc41b82f15c59b689708e5b8e161ddb03e42768ac080022583

--- apps/tor_cert/lib/rsa_ed25519/rsa_ed25519.ex
+++ apps/tor_cert/lib/rsa_ed25519/rsa_ed25519.ex
@@ -9,6 +9,15 @@ defmodule TorCert.RsaEd25519 do
             expiration_date: nil,
             signature: nil
 
+  defp decode_expiration_date(expiration_date) do
+    expiration_date = expiration_date * 60 * 60
+    DateTime.from_unix!(expiration_date)
+  end
+
+  defp encode_expiration_date(expiration_date) do
+    <<div(div(DateTime.to_unix(expiration_date), 60), 60)::32>>
+  end
+
   @doc """
   Fetches the first RSA->Ed25519 cross-certificate in a binary.
 
@@ -18,13 +27,14 @@ defmodule TorCert.RsaEd25519 do
   def fetch(data) do
     <<ed25519_key::binary-size(32), data::binary>> = data
     <<expiration_date::32, data::binary>> = data
+    expiration_date = decode_expiration_date(expiration_date)
     <<siglen::8, data::binary>> = data
     <<signature::binary-size(siglen), data::binary>> = data
 
     {
       %TorCert.RsaEd25519{
         ed25519_key: ed25519_key,
-        expiration_date: DateTime.from_unix!(expiration_date),
+        expiration_date: expiration_date,
         signature: signature
       },
       data
@@ -38,7 +48,7 @@ defmodule TorCert.RsaEd25519 do
   """
   def encode(cert) do
     <<cert.ed25519_key::binary-size(32)>> <>
-      <<DateTime.to_unix(cert.expiration_date)::32>> <>
+      encode_expiration_date(cert.expiration_date) <>
       <<byte_size(cert.signature)>> <>
       cert.signature
   end
