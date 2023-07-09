a92b3c4e2645f55533e0d6df69562b8df03d1cf8
engler <me@emilengler.com>
2023-07-09 17:54:33 +0000

    Implement the TorCell.Versions module using typespec.
    FossilOrigin-Name: 5e3bca07a22083cba3f10768ec5ef27582c81e13cee51de92b5fcea1b7f79708

---
+++ apps/tor_cell/lib/versions.ex
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: ISC
+
+defmodule TorCell.Versions do
+  @enforce_keys [:versions]
+  defstruct versions: nil
+
+  @type t :: %TorCell.Versions{versions: list()}
+
+  @spec decode(binary()) :: TorCell.Versions
+  def decode(payload) do
+    %TorCell.Versions{
+      versions:
+        for <<x, y <- payload>> do
+          <<z::16>> = <<x, y>>
+          z
+        end
+    }
+  end
+
+  @spec encode(TorCell.Versions) :: binary()
+  def encode(cell) do
+    Enum.join(Enum.map(cell.versions, fn z -> <<z::16>> end))
+  end
+end
