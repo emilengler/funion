9e4cf09c0692ae11a3b8f66d5c2c3b9324fa2272
engler <me@emilengler.com>
2023-07-13 18:14:36 +0000

    Do not use a custom struct for the digest.
    FossilOrigin-Name: d2f345ebf08fd412e71d4c131cfffaedfa1f5dc7f9bfc13b057f70ba8b564ca0

--- apps/tor_crypto/lib/digest.ex
+++ apps/tor_crypto/lib/digest.ex
@@ -4,16 +4,15 @@ defmodule TorCrypto.Digest do
   @moduledoc """
   Implements the running digest in a stateful fashion.
   """
-  defstruct state: nil
 
-  @type t :: %TorCrypto.Digest{state: :crypto.hash_state()}
+  @type t :: :crypto.hash_state()
 
   @doc """
   Initializes the digest using a seed.
   """
   @spec init(binary()) :: t()
   def init(seed) do
-    update(%TorCrypto.Digest{state: :crypto.hash_init(:sha)}, seed)
+    update(:crypto.hash_init(:sha), seed)
   end
 
   @doc """
@@ -23,13 +22,13 @@ defmodule TorCrypto.Digest do
   """
   @spec update(t(), binary()) :: t()
   def update(digest, data) do
-    %TorCrypto.Digest{state: :crypto.hash_update(digest.state, data)}
+    :crypto.hash_update(digest, data)
   end
 
   @doc """
   Returns a binary corresponding to the current SHA-1 hash of the digest.
   """
   def calculate(digest) do
-    :crypto.hash_final(digest.state)
+    :crypto.hash_final(digest)
   end
 end
