5dee38cc61e33f9cfb3655fd6770664688fb4398
engler <me@emilengler.com>
2023-07-14 11:24:23 +0000

    Introduce typespec into the TorCert.Ed25519 certificates.
    FossilOrigin-Name: ab3a3014cd2b9ac86383195d1ce5ce2946b4544d2e74f35abd1a6b95b6a2ae5b

--- apps/tor_cert/lib/ed25519/ed25519.ex
+++ apps/tor_cert/lib/ed25519/ed25519.ex
@@ -5,6 +5,14 @@ defmodule TorCert.Ed25519 do
   Provides features for handling with the Ed25519 certificates specified
   in `cert-spec.txt`.
   """
+  @enforce_keys [
+    :cert_type,
+    :expiration_date,
+    :cert_key_type,
+    :certified_key,
+    :extensions,
+    :signature
+  ]
   defstruct cert_type: nil,
             expiration_date: nil,
             cert_key_type: nil,
@@ -12,6 +20,18 @@ defmodule TorCert.Ed25519 do
             extensions: nil,
             signature: nil
 
+  @type t :: %TorCert.Ed25519{
+          cert_type: cert_type(),
+          expiration_date: DateTime.t(),
+          cert_key_type: cert_key_type(),
+          certified_key: binary(),
+          extensions: list(TorCert.Ed25519.Extension.t()),
+          signature: binary()
+        }
+  @type cert_type :: :ed25519_id_signing | :tls_ed25519_signing | :ed25519_auth_ed25519_signing
+  @type cert_key_type :: :ed25519 | :sha256_x509
+
+  @spec decode_cert_type(integer()) :: cert_type()
   defp decode_cert_type(cert_type) do
     case cert_type do
       0x04 -> :ed25519_signing_id
@@ -20,11 +40,13 @@ defmodule TorCert.Ed25519 do
     end
   end
 
+  @spec decode_expiration_date(integer()) :: DateTime.t()
   defp decode_expiration_date(expiration_date) do
     expiration_date = expiration_date * 60 * 60
     DateTime.from_unix!(expiration_date)
   end
 
+  @spec decode_cert_key_type(integer()) :: cert_key_type()
   defp decode_cert_key_type(cert_key_type) do
     case cert_key_type do
       0x01 -> :ed25519
@@ -32,6 +54,7 @@ defmodule TorCert.Ed25519 do
     end
   end
 
+  @spec encode_cert_type(cert_type()) :: binary()
   defp encode_cert_type(cert_type) do
     case cert_type do
       :ed25519_signing_id -> <<0x04>>
@@ -40,10 +63,12 @@ defmodule TorCert.Ed25519 do
     end
   end
 
+  @spec encode_expiration_date(DateTime.t()) :: binary()
   defp encode_expiration_date(expiration_date) do
     <<div(div(DateTime.to_unix(expiration_date), 60), 60)::32>>
   end
 
+  @spec encode_cert_key_type(cert_key_type()) :: binary()
   defp encode_cert_key_type(cert_key_type) do
     case cert_key_type do
       :ed25519 -> <<0x01>>
@@ -51,10 +76,13 @@ defmodule TorCert.Ed25519 do
     end
   end
 
+  @spec encode_extensions(list(TorCert.Ed25519.Extension.t())) :: binary()
   defp encode_extensions(extensions) do
     Enum.join(Enum.map(extensions, fn x -> TorCert.Ed25519.Extension.encode(x) end))
   end
 
+  @spec fetch_extensions(list(TorCert.Ed25519.Extension.t()), integer(), binary()) ::
+          {list(TorCert.Ed25519.Extension.t()), binary()}
   defp fetch_extensions(certs, n, data) when n > 0 do
     {cert, data} = TorCert.Ed25519.Extension.fetch(data)
     fetch_extensions(certs ++ [cert], n - 1, data)
@@ -67,6 +95,7 @@ defmodule TorCert.Ed25519 do
   @doc """
   Decodes an Ed25519 into its internal representation.
   """
+  @spec decode(binary()) :: t()
   def decode(data) do
     remaining = data
     <<1, remaining::binary>> = remaining
@@ -94,6 +123,7 @@ defmodule TorCert.Ed25519 do
   Returns the internal representation of the found certificate, alongside
   the remaining data.
   """
+  @spec fetch(binary()) :: {t(), binary()}
   def fetch(data) do
     # TODO: Consider fetching the version?
     <<1, data::binary>> = data
@@ -123,6 +153,7 @@ defmodule TorCert.Ed25519 do
 
   Returns a binary corresponding to the certificate.
   """
+  @spec encode(t()) :: binary()
   def encode(cert) do
     <<1>> <>
       encode_cert_type(cert.cert_type) <>
@@ -137,6 +168,7 @@ defmodule TorCert.Ed25519 do
   @doc """
   Validates if a certificate is properly signed.
   """
+  @spec is_valid?(t(), binary(), DateTime.t()) :: boolean()
   def is_valid?(cert, key, time \\ DateTime.utc_now()) do
     if DateTime.compare(time, cert.expiration_date) == :gt do
       false
--- apps/tor_cert/lib/ed25519/extension.ex
+++ apps/tor_cert/lib/ed25519/extension.ex
@@ -8,10 +8,20 @@ defmodule TorCert.Ed25519.Extension do
   This module only implements the parts of the specification needed to parse
   the Signed-with-ed25519-key extension. This is intentional.
   """
+  @enforce_keys [:type, :flags, :data]
   defstruct type: nil,
             flags: nil,
             data: nil
 
+  @type t :: %TorCert.Ed25519.Extension{
+          type: type(),
+          flags: flags(),
+          data: binary()
+        }
+  @type type :: :signed_with_ed25519_key
+  @type flags :: nil | :affects_validation
+
+  @spec decode_flags(integer()) :: flags()
   defp decode_flags(flags) do
     case flags do
       0 -> nil
@@ -19,6 +29,7 @@ defmodule TorCert.Ed25519.Extension do
     end
   end
 
+  @spec encode_flags(flags()) :: binary()
   defp encode_flags(flags) do
     case flags do
       nil -> <<0>>
@@ -32,6 +43,7 @@ defmodule TorCert.Ed25519.Extension do
   Returns the internal representation of the found extension, alongside
   the remaining data.
   """
+  @spec fetch(binary()) :: {t(), binary()}
   def fetch(data) do
     <<32::16, data::binary>> = data
     <<0x04, data::binary>> = data
@@ -53,6 +65,7 @@ defmodule TorCert.Ed25519.Extension do
 
   Returns a binary corresponding to the binary representation of the extensions.
   """
+  @spec encode(t()) :: binary()
   def encode(extension) do
     <<32::16>> <> <<0x04>> <> encode_flags(extension.flags) <> extension.data
   end
