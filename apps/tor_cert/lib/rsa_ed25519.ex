32acb7051614799c8d388cac46d1b7765536b970
engler <me@emilengler.com>
2023-07-14 11:51:32 +0000

    Implement typespec for the TorCert.RsaEd25519 module.
    FossilOrigin-Name: fe76e0f92e7cc0ec5baf736b302d6107871a61ed56ccedf920a1de46b9a005b3

--- apps/tor_cert/lib/rsa_ed25519.ex
+++ apps/tor_cert/lib/rsa_ed25519.ex
@@ -5,15 +5,24 @@ defmodule TorCert.RsaEd25519 do
   Provides features for handling with the RSA->Ed25519 cross-certificates
   specified in `cert-spec.txt`.
   """
+  @enforce_keys [:ed25519_key, :expiration_date, :signature]
   defstruct ed25519_key: nil,
             expiration_date: nil,
             signature: nil
 
+  @type t :: %TorCert.RsaEd25519{
+          ed25519_key: binary(),
+          expiration_date: DateTime.t(),
+          signature: binary()
+        }
+
+  @spec decode_expiration_date(integer()) :: DateTime.t()
   defp decode_expiration_date(expiration_date) do
     expiration_date = expiration_date * 60 * 60
     DateTime.from_unix!(expiration_date)
   end
 
+  @spec encode_expiration_date(DateTime.t()) :: binary()
   defp encode_expiration_date(expiration_date) do
     <<div(div(DateTime.to_unix(expiration_date), 60), 60)::32>>
   end
@@ -21,6 +30,7 @@ defmodule TorCert.RsaEd25519 do
   @doc """
   Decodes an RSA->Ed25519 cross-certificate into its internal representation.
   """
+  @spec decode(binary()) :: t()
   def decode(data) do
     remaining = data
     <<ed25519_key::binary-size(32), remaining::binary>> = remaining
@@ -42,6 +52,7 @@ defmodule TorCert.RsaEd25519 do
   Returns the internal representation of the found certificate, alongside
   the remaining data.
   """
+  @spec fetch(binary()) :: {t(), binary()}
   def fetch(data) do
     <<ed25519_key::binary-size(32), data::binary>> = data
     <<expiration_date::32, data::binary>> = data
@@ -64,6 +75,7 @@ defmodule TorCert.RsaEd25519 do
 
   Returns a binary corresponding to the certificate.
   """
+  @spec encode(t()) :: binary()
   def encode(cert) do
     <<cert.ed25519_key::binary-size(32)>> <>
       encode_expiration_date(cert.expiration_date) <>
@@ -74,6 +86,7 @@ defmodule TorCert.RsaEd25519 do
   @doc """
   Validates if a certificate is properly signed.
   """
+  @spec is_valid?(t(), binary(), DateTime.t()) :: boolean()
   def is_valid?(cert, key, time \\ DateTime.utc_now()) do
     {:RSAPublicKey, modulus, exponent} = key
 
