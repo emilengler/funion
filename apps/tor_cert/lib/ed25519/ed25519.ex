6163d46cc15c5cf49c334b5a5db4613e443f4995
engler <me@emilengler.com>
2023-07-14 11:25:00 +0000

    Remove the already deprecated TorCert.Ed25519.fetch/1 function.
    FossilOrigin-Name: 6c4948d5e684eae8b6c4eb65b86dcfcf2a3707f1db6b97bce98f96ec2c1414fb

--- apps/tor_cert/lib/ed25519/ed25519.ex
+++ apps/tor_cert/lib/ed25519/ed25519.ex
@@ -118,37 +118,6 @@ defmodule TorCert.Ed25519 do
   end
 
   @doc """
-  Fetches the first Ed25519 certificate in a binary.
-
-  Returns the internal representation of the found certificate, alongside
-  the remaining data.
-  """
-  @spec fetch(binary()) :: {t(), binary()}
-  def fetch(data) do
-    # TODO: Consider fetching the version?
-    <<1, data::binary>> = data
-    <<cert_type, data::binary>> = data
-    <<expiration_date::32, data::binary>> = data
-    <<cert_key_type, data::binary>> = data
-    <<certified_key::binary-size(32), data::binary>> = data
-    <<n_extensions, data::binary>> = data
-    {extensions, data} = fetch_extensions([], n_extensions, data)
-    <<signature::binary-size(64), data::binary>> = data
-
-    {
-      %TorCert.Ed25519{
-        cert_type: decode_cert_type(cert_type),
-        expiration_date: decode_expiration_date(expiration_date),
-        cert_key_type: decode_cert_key_type(cert_key_type),
-        certified_key: certified_key,
-        extensions: extensions,
-        signature: signature
-      },
-      data
-    }
-  end
-
-  @doc """
   Encodes an Ed25519 certificate into a binary
 
   Returns a binary corresponding to the certificate.
