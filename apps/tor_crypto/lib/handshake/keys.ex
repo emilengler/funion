1ae9704eb29972504adf698b79ce641338d3c282
engler <me@emilengler.com>
2023-07-14 09:11:42 +0000

    Implement typespec in the TorCrypto.Handshake.Keys module.
    FossilOrigin-Name: 30f6cd65551ce9eabef2448d3e8d2f40dedf0294be95938b4dc9d33996df5562

--- apps/tor_crypto/lib/handshake/keys.ex
+++ apps/tor_crypto/lib/handshake/keys.ex
@@ -1,8 +1,16 @@
 # SPDX-License-Identifier: ISC
 
 defmodule TorCrypto.Handshake.Keys do
+  @enforce_keys [:df, :db, :kf, :kb]
   defstruct df: nil,
             db: nil,
             kf: nil,
             kb: nil
+
+  @type t :: %TorCrypto.Handshake.Keys{
+          df: TorCrypto.Digest.t(),
+          db: TorCrypto.Digest.t(),
+          kf: TorCrypto.OnionStream.t(),
+          kb: TorCrypto.OnionStream.t()
+        }
 end
