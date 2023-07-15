3d00721505919735af9c8acf11e7c21762734b75
engler <me@emilengler.com>
2023-07-15 13:26:51 +0000

    Begin work on the tor_proto rewrite.
    FossilOrigin-Name: 2683e485f291962c901dc8ffa4c1b114b9d71cbb9c6b1754751e7f1f09854a83

---
+++ apps/tor_proto/README.md
@@ -0,0 +1,3 @@
+# TorProto
+
+An implementation of the actual Tor protocol, as specified within `tor-spec.txt`.
---
+++ apps/tor_proto/test/tor_proto_test.exs
@@ -0,0 +1,8 @@
+defmodule TorProtoTest do
+  use ExUnit.Case
+  doctest TorProto
+
+  test "greets the world" do
+    assert TorProto.hello() == :world
+  end
+end
