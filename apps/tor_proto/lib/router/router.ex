f60d2e9489f6e155b1ab6831c1277639a6cd2713
engler <me@emilengler.com>
2023-07-20 09:27:36 +0000

    Implement the TorProto.Router module alongside its keys.
    FossilOrigin-Name: d1e2bd3da14134274c1a08221de81a3e634a879636c0d0361d1cf6d575261d9a

---
+++ apps/tor_proto/lib/router/router.ex
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: ISC
+
+defmodule TorProto.Router do
+  @enforce_keys [:identity, :ip4, :orport, :keys]
+  defstruct nickname: nil,
+            identity: nil,
+            ip4: nil,
+            ip6: nil,
+            orport: nil,
+            keys: nil
+
+  @type t :: %TorProto.Router{
+          nickname: String.t(),
+          identity: binary(),
+          ip4: :inet.ip4_address(),
+          ip6: :inet.ip6_address(),
+          orport: integer(),
+          keys: TorProto.Router.Keys.t()
+        }
+end
