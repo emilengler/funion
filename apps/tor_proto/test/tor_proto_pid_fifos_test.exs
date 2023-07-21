512787d60e451883d210f765b93b0854cb6d522d
engler <me@emilengler.com>
2023-07-21 12:27:35 +0000

    Implement the TorProto.PidFifos module, as a CellPool replacement.
    FossilOrigin-Name: 36eb2197c113b93349c0a4b68ee76f89a924110da9b22a58ccbfb375786072a8

---
+++ apps/tor_proto/test/tor_proto_pid_fifos_test.exs
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: ISC
+
+defmodule TorProtoPidFifosTest do
+  use ExUnit.Case
+  doctest TorProto.PidFifos
+
+  test "initializes a TorProto.PidFifos" do
+    assert TorProto.PidFifos.init() == %{}
+  end
+
+  test "enqueues a TorProto.PidFifos" do
+    pid1 = spawn(fn -> nil end)
+    pid2 = spawn(fn -> nil end)
+
+    fifos = TorProto.PidFifos.init()
+    fifos = TorProto.PidFifos.enqueue(fifos, pid1, 1)
+    assert fifos == %{pid1 => {[1], []}}
+    fifos = TorProto.PidFifos.enqueue(fifos, pid2, 1)
+    assert fifos == %{pid1 => {[1], []}, pid2 => {[1], []}}
+    fifos = TorProto.PidFifos.enqueue(fifos, pid1, 2)
+    assert fifos == %{pid1 => {[2], [1]}, pid2 => {[1], []}}
+  end
+
+  test "deques a TorProto.PidFifos" do
+    pid1 = spawn(fn -> nil end)
+    pid2 = spawn(fn -> nil end)
+    fifos = %{pid1 => {[2], [1]}, pid2 => {[1], []}}
+
+    {fifos, cell} = TorProto.PidFifos.dequeue(fifos, pid2)
+    assert fifos == %{pid1 => {[2], [1]}}
+    assert cell == 1
+
+    {fifos, cell} = TorProto.PidFifos.dequeue(fifos, pid2)
+    assert fifos == %{pid1 => {[2], [1]}}
+    assert cell == nil
+
+    {fifos, cell} = TorProto.PidFifos.dequeue(fifos, pid1)
+    assert fifos == %{pid1 => {[], [2]}}
+    assert cell == 1
+
+    {fifos, cell} = TorProto.PidFifos.dequeue(fifos, pid1)
+    assert fifos == %{}
+    assert cell == 2
+
+    {fifos, cell} = TorProto.PidFifos.dequeue(fifos, pid1)
+    assert fifos == %{}
+    assert cell == nil
+  end
+end
