37261994cf1de2c54eecec23e57ef1fd091be9d4
engler <me@emilengler.com>
2023-07-27 15:25:29 +0000

    Begin work on the third-generation TorProto implementation.
    FossilOrigin-Name: 4816007071030feaedb204c7a0c74c893743fa9b2a34f18a5ad0fb3034c4f731

---
+++ apps/tor_proto/lib/tor_proto.ex
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: ISC
+
+defmodule TorProto do
+  @moduledoc """
+  Documentation for `TorProto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TorProto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- apps/tor_proto/mix.exs
+++ apps/tor_proto/mix.exs
@@ -11,7 +11,7 @@ defmodule TorProto.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       license: ["ISC"]
@@ -21,14 +21,16 @@ defmodule TorProto.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl]
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tor_cell, in_umbrella: true}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end
